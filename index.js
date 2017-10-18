75e2362e60df03b14875158be323ce91e04b138c
Dorian <dc@holimotion.com>
2017-10-18 07:40:28 +0200

    add 404

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ ParseServer.createLiveQueryServer(httpServer);
 app.post('/ephemeral_keys', (req, res) => {
   const stripe_version = req.query.api_version;
   if (!stripe_version) {
-    res.status(400).json({'stripe_version':stripe_version});
+    res.status(404).end();
     return;
   }
   // This function assumes that some previous middleware has determined the
