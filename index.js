1a155daa2509f33868b2397b07ca395c6d36cab6
Dorian <dc@holimotion.com>
2017-10-19 07:07:56 +0200

    fix query with body

--- index.js
+++ index.js
@@ -63,9 +63,7 @@ ParseServer.createLiveQueryServer(httpServer);
 
 app.post('/ephemeral_keys', (req, res) => {
 
-  console.log(req.query);
-  console.log(req.body);
-  var stripe_version = req.query.api_version;
+  var stripe_version = req.body.api_version;
   console.log('stripe_version ' + stripe_version + '.');
   if (!stripe_version) {
     res.status(404).end();
