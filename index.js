5a3215ffaef96a0597e3fbe4c4227f57c0328500
Dorian <dc@holimotion.com>
2017-10-19 08:24:41 +0200

    add parameters for ephemeralKeys

--- index.js
+++ index.js
@@ -61,7 +61,8 @@ httpServer.listen(port, function() {
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
 
-app.post('/ephemeral_keys', (req, res) => {
+
+app.post('/stripe/ephemeral_keys', (req, res) => {
 
   var stripe_version = req.body.api_version;
   console.log('stripe_version ' + stripe_version + '.');
@@ -79,4 +80,41 @@ app.post('/ephemeral_keys', (req, res) => {
   }).catch((err) => {
     res.status(500).end();
   });
+});
+
+
+app.post('/stripe/ephemeral_keys', (req, res) => {
+
+  var stripe_version = req.body.api_version;
+  var stripe_token = req.body.token;
+  var user_email = req.body.email;
+  console.log('stripe_version ' + stripe_version + '.');
+  if (!stripe_version || !stripe_token  || !user_email) {
+    res.status(404).end();
+    return;
+  }
+
+  stripe.customers.create({
+    email:user_email
+    description: 'Customer for '+ user_email,
+    source: stripe_token // obtained with Stripe.js
+  
+  }, function(err, customer) {
+    // asynchronously called
+    if (!customer) {
+      res.status(400).end();
+      return;
+    }
+    stripe.ephemeralKeys.create(
+      {customer: customer.id},
+      {stripe_version: stripe_version}
+    ).then((key) => {
+      res.status(200).json(key);
+    }).catch((err) => {
+      res.status(500).end();
+    });
+  });
+  // This function assumes that some previous middleware has determined the
+  // correct customerId for the session and saved it on the request object.
+  
 });
