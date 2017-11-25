82d8f716eae51c3e6df2e4525ade5589fa159870
Dorian <dc@holimotion.com>
2017-11-25 17:52:19 +0100

    new test

--- cloud/main.js
+++ cloud/main.js
@@ -3,25 +3,39 @@ Parse.Cloud.define('hello', function(req, res) {
 
 	// request has 2 parameters: params passed by the client and the authorized user 
 	var params = req.params;
-	var receiver = params.receiver;
+	var receiver = req.sender.receiver;
+  	var messageText = params.text; 
+  	
+	var query = new Parse.Query(Parse.User);
+	query.equalTo('objectId',receiver)
 
-	var pushQuery = new Parse.Query(Parse.Installation);
-  	pushQuery.equalTo('user', receiver); // targeting receiver devices only   
 
-  	// Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  	var messageText = params.text;
-
-  	Parse.Push.send({
-    where: pushQuery, // Set our Installation query                                                                                                                                                              
-    data: {
-      alert: "Message: " + messageText
-     }
-  	}, { success: function() {
-      console.log("#### PUSH OK");
-  	 }, error: function(error) {
-      console.log("#### PUSH ERROR" + error.message);
-   	 }, useMasterKey: true});
-
-  	console.log(req.params);
-  	res.success('Hi');
+	query.find({
+  		success: function(results) { 
+  			alert("Successfully retrieved " + results.length + " scores.");
+    		// Do something with the returned Parse.Object values
+    		for (var i = 0; i < results.length; i++) {
+      			var object = results[i];
+      			var pushQuery = new Parse.Query(Parse.Installation);
+  				pushQuery.equalTo('user', object); // targeting iOS devices only  
+  				// Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  				Parse.Push.send({
+                where: query,
+                data: {
+                    "alert": messageText,
+                    "content-available": 1,
+                },
+                }, {
+                    success: function() {
+                        console.log('##### PUSH OK ');
+                    },
+                    error: function(error) {
+                        console.log('##### PUSH ERROR ');
+                    },
+                    useMasterKey: true
+                });
+    		}
+  		},
+  		error: function(error) { alert("Error: " + error.code + " " + error.message); }
+	});
 });
