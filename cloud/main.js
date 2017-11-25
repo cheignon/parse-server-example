72dc51b36841aad77f78e5921860c339261f369b
Dorian <dc@holimotion.com>
2017-11-25 17:06:22 +0100

    tesst cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,8 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
 
-Parse.Cloud.define('push', function(request, response) {
-
-  var messageText = request.object.get('message');
-  var usersReceived = request.object.get('receiver');
+  var messageText = req.object.get('message');
+  var usersReceived = req.object.get('receiver');
 	console.log(usersReceived);
 	console.log(messageText);
   res.success('Hi');
