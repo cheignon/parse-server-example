c85a89ddd3f416e431c5aecb5358fd538d7072ea
Dorian <dc@holimotion.com>
2017-11-25 16:58:04 +0100

    add config var

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,11 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define("push", function(request, response) {
-	console.log(request);
-  res.success(request);
+Parse.Cloud.define('push', function(request, response) {
+
+  var messageText = request.object.get('message');
+  var usersReceived = request.object.get('receiver');
+	console.log(usersReceived);
+	console.log(messageText);
+  res.success('Hi');
 });
