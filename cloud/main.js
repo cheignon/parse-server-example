285dd10bd73cf9a541dc1677caa31bfccdb873fe
Dorian <dc@holimotion.com>
2017-11-25 18:44:52 +0100

    another test

--- cloud/main.js
+++ cloud/main.js
@@ -6,30 +6,19 @@ Parse.Cloud.define('hello', function(req, res) {
 	var receiver = params.receiver;
 
 
-	var pushQuery = new Parse.Query(Parse.Installation);
-  	pushQuery.equalTo('user', receiver); // targeting iOS devices only   
-
-  	// Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
-  	var messageText = params.text;
-	console.log("receiver "+receiver);
-	console.log("messageText "+messageText);
-  	Parse.Push.send({
-        where: pushQuery,
-        data: {
-          alert: 'Testing .....',
-          badge: 1,
-          sound: 'default'
-        }
-      }, {
-        useMasterKey: true,
-        success: function() {
-          console.log('Push sent!');
-        },
-        error: function(error) {
-          console.log('There was a problem :( ' + error);
-        }
-      });
-
-  	console.log(req.params);
-  	res.success('Hi');
+	Parse.Cloud.httpRequest({ 
+  		url: "https://onesignal.com/api/v1/notifications", 
+ 		method: "POST", 
+  		headers: { 
+    		"Content-Type": "application/json;charset=utf-8", 
+    		"Authorization": "Basic "+process.env.ONE_SIGNAL_APP_KEY 
+  		}, 
+  		body: JSON.stringify(jsonBody), 
+  		success: function(httpResponse) { 
+   			res.success();
+  		}, 
+  		error: function(httpResponse) { 
+    		res.success('Failed with: ' + httpResponse.status); 
+  		} 
+	});
 });
