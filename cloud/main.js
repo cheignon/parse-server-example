78f9e4d64020f9cf70a6d4c54d6a6a7c1ca2ff37
Dorian <dc@holimotion.com>
2017-11-25 17:26:11 +0100

    add test installation

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,27 @@
 
 Parse.Cloud.define('hello', function(req, res) {
 
+	// request has 2 parameters: params passed by the client and the authorized user 
+	var params = req.params;
+	var receiver = req.sender.receiver;
 
-  console.log(req.params);
-  res.success('Hi');
+	var pushQuery = new Parse.Query(Parse.Installation);
+  	pushQuery.equalTo('user', receiver); // targeting receiver devices only   
+
+  	// Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  	var messageText = params.text;
+
+  	Parse.Push.send({
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    data: {
+      alert: "Message: " + messageText
+     }
+  	}, { success: function() {
+      console.log("#### PUSH OK");
+  	 }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+   	 }, useMasterKey: true});
+
+  	console.log(req.params);
+  	res.success('Hi');
 });
