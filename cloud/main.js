a944492278c94ac02bc7d77c26160ac7a9fc25ac
dorian cheignon <cheignon.d@gmail.com>
2017-11-26 16:02:00 +0100

    trying to fix tag

--- cloud/main.js
+++ cloud/main.js
@@ -7,7 +7,7 @@ Parse.Cloud.define('hello', function(req, res) {
   var sender_name = params.sender_name;
   var date = params.date;
   var job = params.job;
-  
+
   var text_en = '{{ sender | default: "someone" }} needs an help on {{ day_request | default: "today" }} for {{ job_request | default: "work" }}'
   var text_fr = '{{ sender | default: "Quelqu\'un" }} a besoin de d\'aide pour le {{ day_request | default: "today" }} concernant {{ job_request | default: "work" }}'
 
@@ -15,7 +15,11 @@ Parse.Cloud.define('hello', function(req, res) {
   		app_id: process.env.ONE_SIGNAL_APP_ID,
   		contents: {"en": text_en,"fr":text_fr},
   		include_player_ids: [receiver],
-      tags:{"sender":sender_name,"day_request":date,"job_request":job},
+      tags:[
+              {"field": "tag", "key": "sender", "relation": "=", "value": sender_name},
+              {"field": "tag", "key": "day_request", "relation": "=", "value": date},
+              {"field": "tag", "key": "job_request", "relation": "=", "value": job},
+          ],
 	};
 	sendNotification(message);
 });
